0fb3143624bc9f9706fac1397349fbac2404b73a
Lucas Santos <lhs.santoss@gmail.com>
2024-02-09 17:08:28 +0100

    fix(buildall): fix build all command location

--- sources/buildAll.sh
+++ sources/buildAll.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+HERE=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
 while IFS= read -r -d $'\0' dir; do
     dir_base=$(basename "$dir")
-    if [[ "$dir_base" != "assets" && "$dir_base" != "base-template" && "$dir_base" != "." ]]; then
+    if [[ "$dir_base" != "assets" && "$dir_base" != "base-template" && "$dir_base" != "." && "$dir_base" != "sources" ]]; then
         echo "Building $dir_base"
         (cd "$dir" && npm ci && npm run build && npm run export)
     fi
-done < <(find . -maxdepth 1 -type d -print0)
+done < <(find $HERE -maxdepth 1 -type d -print0)
